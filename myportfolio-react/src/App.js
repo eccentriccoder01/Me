a20bdefae99097678293a238c50bb511a962a7d3
eccentriccoder01 <sagnikgraviton847@gmail.com>
2023-11-08 00:13:59 +0530

    Updated

--- myportfolio-react/src/App.js
+++ myportfolio-react/src/App.js
@@ -1,4 +1,10 @@
-import logo from './logo.svg';
+import {BrowserRouter as Router, Route, Routes} from 'react-router-dom';
+import About from './Pages/About';
+import Skills from './Pages/Skills';
+import Certifications from './Pages/Certifications';
+import Education from './Pages/Education';
+import Projects from './Pages/Projects';
+import Experience from './Pages/Experience';
 import './App.css';
 
 function App() {
